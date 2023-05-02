950a50409fc92e8c39ae85cef08c8ac12f147d5b
mrfish <misterfish1993@gmail.com>
2023-05-02 18:12:48 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <header className='header'>
+            <img
+                src={'https://is2-ssl.mzstatic.com/image/thumb/Purple124/v4/7d/0e/13/7d0e1372-d04f-29b1-88aa-9f9e729983a7/source/256x256bb.jpg'}/>
+        </header>
+    )
+}
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function Profile() {
+    return (
+        <div className='content'>
+            <img
+                src={'https://marketplace.canva.com/EAD2962NKnQ/2/0/1600w/canva-rainbow-gradient-pink-and-purple-virtual-background-_Tcjok-d9b4.jpg'}/>
+            <div>ava+descriptions</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
